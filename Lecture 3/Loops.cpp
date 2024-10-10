cc797613968a130c8385e29c541dca0187fd1681
SaddamHussain157 <saddamhussainsumalani109@gmail.com>
2024-10-10 21:46:12 +0500

    much more on loops

--- Lecture 3/Loops.cpp
+++ Lecture 3/Loops.cpp
@@ -1,13 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-  
+int main()
+{
+
   for (int i = 0; i < 5; i++)
   {
     cout << i << endl;
-    
-    }
+  }
+  // While loop print numbers from 10 to 20
+  int count = 10;
+  // while (count <= 20)
+  // {
+  //   cout << count << endl;
+  //   count++;
+  // }
+  // Using the user input of the loop 
+  int n;
+  cout << "Enter how many times a nuber print: ";
+  cin >> n;
+  while (count <= n)
+  {
+    cout << count;
+    count++;
+  }
 
-    
-    return 0;
+  return 0;
 }
