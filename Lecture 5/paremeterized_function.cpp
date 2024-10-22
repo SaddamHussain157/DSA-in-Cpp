18d7d3e7bcfd8250bd1f92461122cddf7b35f621
SaddamHussain157 <saddamhussainsumalani109@gmail.com>
2024-10-22 18:07:29 +0500

    working on functions

--- Lecture 5/paremeterized_function.cpp
+++ Lecture 5/paremeterized_function.cpp
@@ -8,9 +8,29 @@
 using namespace std;
 int paraFunction(int a, int b)
 {
-    return a + b;
+    int sum = a + b;
+    return sum;
+}
+int sum(int a, int b)
+{
+    int s = a + b;
+    return s;
+}
+int minimum_of_two(int a, int b) //which we are giving the values that are parameters
+{
+    if (a < b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
 }
 int main()
 {
-    cout << "the function returned Value: " << paraFunction(5, 5) << endl;
+
+    //     cout << "the function returned Value: " << paraFunction(5, 5) << endl;
+    //     cout << "sum function: " << sum(10, 5);
+    cout << "MiniMum of two Numbers" << "Whereas the minimun value is: " << minimum_of_two(2, 77); //The values which we are passing that are called: arguments.
 }
