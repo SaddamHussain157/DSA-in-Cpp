133f174a0b4860a842969c30d47c558156ca69ef
SaddamHussain157 <saddamhussainsumalani109@gmail.com>
2024-10-24 17:31:39 +0500

    practice of the patterns

--- Lecture 5/paremeterized_function.cpp
+++ Lecture 5/paremeterized_function.cpp
@@ -16,7 +16,7 @@ int sum(int a, int b)
     int s = a + b;
     return s;
 }
-int minimum_of_two(int a, int b) //which we are giving the values that are parameters
+int minimum_of_two(int a, int b) // which we are giving the values that are parameters
 {
     if (a < b)
     {
@@ -27,10 +27,16 @@ int minimum_of_two(int a, int b) //which we are giving the values that are param
         return b;
     }
 }
+double sumofDoubleV(double a, double b)
+{
+    return a + b;
+}
 int main()
+
 {
 
+    cout << "Sum of Decimal Values: " << sumofDoubleV(21.1, 12.44);
     //     cout << "the function returned Value: " << paraFunction(5, 5) << endl;
     //     cout << "sum function: " << sum(10, 5);
-    cout << "MiniMum of two Numbers" << "Whereas the minimun value is: " << minimum_of_two(2, 77); //The values which we are passing that are called: arguments.
+    // cout << "MiniMum of two Numbers" << "Whereas the minimun value is: " << minimum_of_two(2, 77); //The values which we are passing that are called: arguments.
 }
