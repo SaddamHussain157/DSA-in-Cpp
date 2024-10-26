22204bcbe301ccc7a4b5f03acc3a9c8a4314c981
SaddamHussain157 <saddamhussainsumalani109@gmail.com>
2024-10-26 16:45:42 +0500

    Sizes of the datatypes

---
+++ size_checkOf_variable.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << sizeof(int) << endl;
+    cout << sizeof(long int) << endl;
+    cout << sizeof(long double) << endl;
+
+    cout << sizeof(long long) << endl;
+    cout << sizeof(short) << endl;
+    cout << sizeof(long signed) << endl;
+    cout << sizeof(unsigned) << endl;
+}
